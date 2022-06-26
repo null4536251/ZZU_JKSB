c8e471ef6eab22a18e89960bd1c36f1e32ac417f
candlc <58896779+candlc@users.noreply.github.com>
2022-06-27 01:26:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,76 @@
 import os
+
+os.environ["http_proxy"] = "127.0.0.1:4973"
 import re
 import smtplib
 import traceback
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+
 import requests
+
 from fake_useragent import UserAgent
 
-
-os.environ["http_proxy"] = "127.0.0.1:4973"
-
 LOGIN_URL = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login'
-JKSB_URL = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
-
-ua = UserAgent(path="fake_useragent_0.1.11.json")
-headers = {'User-Agent': ua.random}
-
+SECOND_URL = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0?fun2=&door='
+THIRD_URL = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
 username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
-data = {'uid': username, 
-        'upw': password
-       }
-
-sev = os.environ["SEV"]
-
+ua = UserAgent(path="fake_useragent_0.1.11.json")
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51'}
+header['User-Agent'] = ua.random
+s = requests.Session()
+data = {'uid': username, 'upw': password}
 mail_user = os.environ["MAIL_USER"]  # QQ邮箱账户
 mail_pass = os.environ["MAIL_PASS"]  # QQ邮箱授权码
 receivers = os.environ["RECEIVERS"]  # QQ邮箱账户
 
 try:
-    session = requests.Session()
-    response = session.post(LOGIN_URL, headers=headers, data=data)
-
-    #print(response.text)
-    url = re.findall(r'location="(.*?)"', response.text)[0]
-    #print('登录成功后的第一个url', url)
+    response = s.post(LOGIN_URL, headers=header, data=data)
+    response_html = str(response.content.decode('utf-8'))
+    result = re.findall(r'ptopid=(.*?)&sid=(.*?)"', response_html)
+    ptopid = result[0][0]
+    sid = result[0][1]
     response.close()
-
-    frameUrl = url.replace('first6', 'jksb') + '&fun2='
-
-    response = session.get(frameUrl, headers=headers)
-    new_page = BeautifulSoup(response.text, "html.parser")
-    div = new_page.find('div', attrs={'id': 'bak_0'})
-
-    did = div.find('input', attrs={'name': 'did'}).get('value')
-    #print('did', did)
-    fun18 = div.find('input', attrs={'name': 'fun18'}).get('value')
-    #print('fun18', fun18)
-    door = div.find('input', attrs={'name': 'door'}).get('value')
-    #print('door', door)
-    sid1 = div.findAll('input', attrs={'name': 'sid'})[0].get('value')
-    #print('sid1', sid1)
-    men6 = div.find('input', attrs={'name': 'men6'}).get('value')
-    #print('men6', men6)
-    ptopid = div.find('input', attrs={'name': 'ptopid'}).get('value')
-    #print('ptopid', ptopid)
-    sid2 = div.findAll('input', attrs={'name': 'sid'})[1].get('value')
-    #print('sid2', sid2)
-
-    data1 = {
-        'did': did,
-        'door': door,
-        'fun18': fun18,
-        'sid': sid1,
-        'men6': men6,
-        'ptopid': ptopid,
-        'sid': sid2
-    }
-
-    response = session.post(JKSB_URL, headers=headers, data=data1)
-    response.encoding = 'utf-8'
-    # print(response.text)
-
-    data2 = {
+    parameter = {'ptopid': ptopid, 'sid': sid}
+    data1 = urlencode(parameter)
+    response = s.post(LOGIN_URL + "?" + data1, headers=header, data=data)
+    response_html = str(response.content.decode('utf-8'))
+    response.close()
+    url = re.findall(r'parent.window.location="(.*?)"}', response_html)[0]
+    response = s.get(url, headers=header)
+    response_html = str(response.content.decode('utf-8'))
+    response.close()
+    # print(response_html)
+    url = re.findall(r'<iframe name="zzj_top_6s" id="zzj_top_6s" src="(.*?)" marginwid', response_html)[0]
+    response = s.get(url, headers=header)
+    response_html = str(response.content.decode('utf-8'))
+    fun18 = re.findall(r'name="fun18" value="(.*?)"', response_html)[0]
+    response.close()
+    data2 = {}
+    data2['ptopid'] = re.findall(r'<input type="hidden" name="ptopid" value="(.*?)">', response_html)[0]
+    data2['sid'] = re.findall(r'<input type="hidden" name="sid" value="(.*?)>', response_html)[0]
+    data2['fun2'] = ''
+    data2['did'] = 1
+    data2['fun18'] = fun18
+    response = s.post(THIRD_URL, headers=header, data=data2)
+    response_html = str(response.content.decode('utf-8'))
+    response.close()
+    data3 = {
         'myvs_1': '否',
         'myvs_2': '否',
         'myvs_3': '否',
         'myvs_4': '否',
         'myvs_5': '否',
+        # 'myvs_6': '否',
         'myvs_7': '否',
         'myvs_8': '否',
-        'myvs_9': '做了',
-        'myvs_11': '否',
+        'myvs_9': '否',
+        'myvs_10': '否',
+        # 'myvs_11': '否',
         'myvs_12': '否',
-        'myvs_13': '否',
-        'myvs_15': '否',
         'did': 2,
         'door': '',
         'day6': 'b',
@@ -94,22 +79,22 @@ try:
         'shi6': '',
         'fun3': '',
         'fun18': fun18,
-        'jingdu': '113.535663',  # 经度 TODO:注意更换
-        'weidu': '34.811384',  # 维度 TODO:注意更换
-        'ptopid': data1['ptopid'],
-        'sid': data1['sid2'],
+        'jingdu': '116.570146',  # 经度 TODO:注意更换
+        'weidu': '39.791691',  # 维度 TODO:注意更换
+        'ptopid': data2['ptopid'],
+        'sid': data2['sid'],
         'myvs_13': 'g',
-        'myvs_13a': '41',  # 省代码 TODO:注意更换
-        'myvs_13b': '4101',  # 市代码 TODO:注意更换
-        'myvs_13c': '郑州市.高新区',  # TODO:注意更换
+        'myvs_13a': '11',  # 省代码 TODO:注意更换
+        'myvs_13b': '1101',  # 市代码 TODO:注意更换
+        'myvs_13c': '北京市.通州区',  # TODO:注意更换
         'myvs_24': '否',
         'myvs_26': 5,
-        'memo22': '郑州大学主校区'
+        'memo22': '成功获取'
     }
-    response = session.post(JKSB_URL, headers=headers, data=data2)
+    response = s.post(THIRD_URL, headers=header, data=data3)
     html = response.content.decode('utf-8')
     res = re.findall(r'(感谢你今日上报健康状况)', html)[0]
-    #print(res)
+    print(res)
     if res == '':
         raise Exception("error!")
     try:
@@ -125,17 +110,13 @@ try:
         sml.login(mail_user, mail_pass)
         sml.sendmail(mail_user, receivers, msg.as_string())
         print("打卡成功, 邮件发送成功")
-        requests.post('https://sctapi.ftqq.com/'+str(sev)+'.send?title=打卡成功')
     except Exception as err:
         print(err)
-        requests.post('https://sctapi.ftqq.com/'+str(sev)+'.send?title=打卡成功但是发送邮件失败')
         print("打卡成功但是发送邮件失败")
 
 except Exception as err:
     print(err)
     traceback.print_exc()
-    requests.post('https://sctapi.ftqq.com/'+str(sev)+'.send?title=打卡失败')
-    
     print('打卡失败')
     msg = MIMEMultipart()
     conntent = "打卡提醒"
